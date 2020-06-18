e91b997d74d3a54f80c51b43949ed33c398b72bc
gp22 <gp22@tutanota.com>
2020-06-18 17:24:07 -0600

    add footer content, styling

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -11,20 +11,21 @@ import postcss from "gulp-postcss";
 import sourcemaps from "gulp-sourcemaps";
 import cleanCss from "gulp-clean-css";
 import gulpif from "gulp-if";
-import purgecss from "gulp-purgecss";
+// import purgecss from "gulp-purgecss";
 
 const PROD = yargs.argv.prod;
 
 export const styles = () => {
-	return src("src/scss/style.scss")
-		.pipe(gulpif(!PROD, sourcemaps.init()))
-		.pipe(sass().on("error", sass.logError))
-		.pipe(postcss([require("tailwindcss")]))
-		.pipe(gulpif(PROD, postcss([require("autoprefixer")])))
-		.pipe(gulpif(PROD, purgecss({ content: ["./**/*.php"] })))
-		.pipe(gulpif(PROD, cleanCss({ compatibility: "ie8" })))
-		.pipe(gulpif(!PROD, sourcemaps.write()))
-		.pipe(dest("public/css"));
+	return (
+		src("src/scss/style.scss")
+			.pipe(gulpif(!PROD, sourcemaps.init()))
+			.pipe(sass().on("error", sass.logError))
+			.pipe(gulpif(PROD, postcss([require("autoprefixer")])))
+			// .pipe(gulpif(PROD, purgecss({ content: ["./**/*.php"] })))
+			.pipe(gulpif(PROD, cleanCss({ compatibility: "ie8" })))
+			.pipe(gulpif(!PROD, sourcemaps.write()))
+			.pipe(dest("public/css"))
+	);
 };
 
 export const dev = () => {
