377b978fa5848305b90458eb51e952133b7036ad
gp22 <gp22@tutanota.com>
2020-06-24 15:45:31 -0600

    hero layout and styling

--- inc/hero.php
+++ inc/hero.php
@@ -19,27 +19,38 @@ add_action( 'genesis_after_header', 'genesis_hero_section_output' );
  */
 function genesis_hero_section_output( ) {
 
-echo <<<HTML
+	if ( is_front_page() ) {
 
-	<section class="hero">
-		<div class="wrap hero-inner mx-auto px-6 py-16 flex flex-col items-center justify-between  lg:flex-row lg:pt-32">
-			<div class="max-w-lg">
-				<h1 class="font-bold font-display mb-2 text-2xl text-white  lg:leading-snug lg:text-5xl xl:text-6xl">
-					Heading
-				</h1>
-				<h2 class="text-white opacity-75 mb-10  lg:mb-0">
-					Subhead
-				</h2>
+		$hero = get_field( 'hero' );
+
+		?>
+
+		<section class="hero">
+			<div class="wrap hero-inner">
+				<div class="hero-heading-container">
+					<h1 class="hero-heading">
+
+						<?php if ( $hero['hero-heading'] ) echo esc_html( $hero['hero-heading'] ); ?>
+
+					</h1>
+					<h2 class="hero-subhead">
+
+						<?php if ( $hero['hero-subhead'] ) echo esc_html( $hero['hero-subhead'] ); ?>
+
+					</h2>
+				</div>
+				<img
+					alt="Paul Garcia"
+					src="<?php echo get_stylesheet_directory_uri(); ?>/public/img/paul_garcia.png"
+					class="hero-img"
+				/>
 			</div>
-			<img
-				src=""
-				class="rounded-full mx-auto z-10 shadow-lg  lg:mx-0"
-			/>
-		</div>
 
-		<svg viewBox="0 0 1366 174" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"> <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g transform="translate(-2.000000, 44.000000)" fill="#FFFFFF" fill-rule="nonzero"> <path d="M0,0 C90.7283404,0.927527913 147.912752,27.187927 291.910178,59.9119003 C387.908462,81.7278826 543.605069,89.334785 759,82.7326078 C469.336065,156.254352 216.336065,153.6679 0,74.9732496" opacity="0.100000001"></path> <path d="M100,104.708498 C277.413333,72.2345949 426.147877,52.5246657 546.203633,45.5787101 C666.259389,38.6327546 810.524845,41.7979068 979,55.0741668 C931.069965,56.122511 810.303266,74.8455141 616.699903,111.243176 C423.096539,147.640838 250.863238,145.462612 100,104.708498 Z" opacity="0.100000001"></path> <path d="M1046,51.6521276 C1130.83045,29.328812 1279.08318,17.607883 1439,40.1656806 L1439,120 C1271.17211,77.9435312 1140.17211,55.1609071 1046,51.6521276 Z" id="Path-4" opacity="0.200000003"></path> </g> <g transform="translate(-4.000000, 76.000000)" fill="#FFFFFF" fill-rule="nonzero"> <path d="M0.457,34.035 C57.086,53.198 98.208,65.809 123.822,71.865 C181.454,85.495 234.295,90.29 272.033,93.459 C311.355,96.759 396.635,95.801 461.025,91.663 C486.76,90.01 518.727,86.372 556.926,80.752 C595.747,74.596 622.372,70.008 636.799,66.991 C663.913,61.324 712.501,49.503 727.605,46.128 C780.47,34.317 818.839,22.532 856.324,15.904 C922.689,4.169 955.676,2.522 1011.185,0.432 C1060.705,1.477 1097.39,3.129 1121.236,5.387 C1161.703,9.219 1208.621,17.821 1235.4,22.304 C1285.855,30.748 1354.351,47.432 1440.886,72.354 L1441.191,104.352 L1.121,104.031 L0.457,34.035 Z"></path> </g> </g> </svg>
-	</section>
+			<svg width="100%" height="50px" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 1920 75"><defs><style>.a{fill:none;}.b{clip-path:url(#a);}.c,.d{fill:#ffffff;}.d{opacity:0.5;isolation:isolate;}</style><clipPath id="a"><rect class="a" width="1920" height="75"></rect></clipPath></defs><title>wave</title><g class="b"><path class="c" d="M1963,327H-105V65A2647.49,2647.49,0,0,1,431,19c217.7,3.5,239.6,30.8,470,36,297.3,6.7,367.5-36.2,642-28a2511.41,2511.41,0,0,1,420,48"></path></g><g class="b"><path class="d" d="M-127,404H1963V44c-140.1-28-343.3-46.7-566,22-75.5,23.3-118.5,45.9-162,64-48.6,20.2-404.7,128-784,0C355.2,97.7,341.6,78.3,235,50,86.6,10.6-41.8,6.9-127,10"></path></g><g class="b"><path class="d" d="M1979,462-155,446V106C251.8,20.2,576.6,15.9,805,30c167.4,10.3,322.3,32.9,680,56,207,13.4,378,20.3,494,24"></path></g><g class="b"><path class="d" d="M1998,484H-243V100c445.8,26.8,794.2-4.1,1035-39,141-20.4,231.1-40.1,378-45,349.6-11.6,636.7,73.8,828,150"></path></g></svg>
+		</section>
 
-HTML;
+		<?php
+
+	}
 
 }
